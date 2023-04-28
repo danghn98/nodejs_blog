a6d617cec3e8f00a354af62dab9e92c802a4a94c
henry-shiftcam <dangch@ohio-digital.com>
2023-04-28 09:12:09 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const path = require("path")
-const express = require("express")
-const morgan = require("morgan")
-const { engine } = require("express-handlebars")
-const app = express()
-const port = 3000
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
 
 // add css and image files
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 // http logger
-app.use(morgan("combined"))
+// app.use(morgan("combined"))
 
 // template engine
 app.engine(
@@ -17,18 +17,22 @@ app.engine(
   engine({
     extname: ".hbs",
   })
-)
-app.set("view engine", "hbs")
-app.set("views", path.join(__dirname, "resources/views"))
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
-  res.render("home")
-})
+  res.render("home");
+});
 
 app.get("/news", (req, res) => {
-  res.render("news")
-})
+  res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
